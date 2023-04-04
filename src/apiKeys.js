88d1cdd528c75a0024f79c484e99dd54ab8f95f6
DHARMENDRA KUMAR <dharmendrakumar1208@gmail.com>
2023-04-04 09:44:15 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "991b6dab44bf81793db32bd8b6f25216",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
